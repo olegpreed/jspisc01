fcff44b3096f7a8df674214c56fba0cd659f229e
Oleg Preed <leo.silveri@mail.ru>
2022-07-17 02:37:28 +0300

    new

--- hardclasses.js
+++ hardclasses.js
@@ -1,5 +1,6 @@
-//classes!
-class Company {
+import { Employee } from "./classes.js";
+
+export class Company {
   constructor(companyName, currentProjects, completedProjects, staff) {
     this.companyName = companyName;
     this.currentProjects = currentProjects;
@@ -30,7 +31,7 @@ class Company {
   }
 }
 
-class Project {
+export class Project {
   constructor(projectName, minQualification, Team) {
     this.projectName = projectName;
     this.minQualification = minQualification;
@@ -52,26 +53,8 @@ class Project {
       this.Team.managers.push(member);
   }
 }
-class Employee {
-  constructor(name, grade, skills, company) {
-    this.name = name;
-    this.grade = grade;
-    this.skills = skills;
-    this.company = company;
-  }
-  changecompany(newCompanyName) {
-    this.company = newCompanyName;
-  }
-  upGrade() {
-    if (this.grade === "L1") this.grade = "L2";
-    else if (this.grade === "L2") this.grade = "L3";
-    else if (this.grade === "L3") this.grade = "L4";
-  }
-  addSkill(newSkillName) {
-    this.skills.push(newSkillName);
-  }
-}
-class FrontendDeveloper extends Employee {
+
+export class FrontendDeveloper extends Employee {
   constructor(name, grade, skills, company, stack, projectQuantity) {
     super(name, grade, skills, company);
     this.stack = stack;
@@ -82,7 +65,8 @@ class FrontendDeveloper extends Employee {
     this.stack.push(newTech);
   }
 }
-class BackendDeveloper extends Employee {
+
+export class BackendDeveloper extends Employee {
   constructor(name, grade, skills, company, stack, projectQuantity) {
     super(name, grade, skills, company);
     this.stack = stack;
@@ -93,7 +77,8 @@ class BackendDeveloper extends Employee {
     this.stack.push(newTech);
   }
 }
-class Manager extends Employee {
+
+export class Manager extends Employee {
   constructor(name, grade, skills, company, projectQuantity) {
     super(name, grade, skills, company);
     this.projectQuantity = projectQuantity;
@@ -103,80 +88,3 @@ class Manager extends Employee {
     else return false;
   }
 }
-
-// frontend, backend, manager objects
-
-let frontendMax = new FrontendDeveloper(
-  "Max",
-  "L1",
-  ["hardworking"],
-  "Unemployed",
-  ["git", "js"],
-  21
-);
-let frontendLeva = new FrontendDeveloper(
-  "Leva",
-  "L3",
-  ["hardworking", "great dancer"],
-  "Vkusvill",
-  ["git", "js", "C++"],
-  21
-);
-let backendOleg = new BackendDeveloper(
-  "Oleg",
-  "L2",
-  ["fluent english"],
-  "Mail.ru",
-  ["nodejs", "git"],
-  10
-);
-let managerSveta = new Manager(
-  "Sveta",
-  "L4",
-  ["woman", "good kisser"],
-  "School21",
-  31
-);
-let managerKatya = new Manager(
-  "Katya",
-  "L3",
-  ["speedrunner", "good chef"],
-  "School21",
-  41
-);
-
-let frontendDima = new Manager(
-  "Dima",
-  "L1",
-  ["hardworking", "great dancer"],
-  "Masterbim",
-  ["git", "js", "C++"],
-  21
-);
-
-// objects!
-let staffTeam = {
-  developers: {
-    frontend: [frontendMax, frontendLeva],
-    backend: [backendOleg],
-  },
-  managers: [managerSveta, managerKatya],
-};
-
-let soLong = new Project("so_long", "L2", staffTeam);
-let miniShell = new Project("minishell", "L3", staffTeam);
-
-let arrProjects = [soLong, miniShell];
-
-let finishedProjects = [];
-
-let school21 = new Company(
-  "21 school",
-  arrProjects,
-  finishedProjects,
-  staffTeam
-);
-
-console.log(frontendMax);
-frontendMax.expandStack("Unity");
-console.log(frontendMax);
---
+++ realobjects.js
@@ -0,0 +1,79 @@
+import { Company, Project, FrontendDeveloper, BackendDeveloper, Manager} from "./hardclasses.js"
+
+// frontend, backend, manager objects
+
+let frontendMax = new FrontendDeveloper(
+	"Max",
+	"L1",
+	["hardworking"],
+	"Unemployed",
+	["git", "js"],
+	21
+  );
+  let frontendLeva = new FrontendDeveloper(
+	"Leva",
+	"L3",
+	["hardworking", "great dancer"],
+	"Vkusvill",
+	["git", "js", "C++"],
+	21
+  );
+  let backendOleg = new BackendDeveloper(
+	"Oleg",
+	"L2",
+	["fluent english"],
+	"Mail.ru",
+	["nodejs", "git"],
+	10
+  );
+  let managerSveta = new Manager(
+	"Sveta",
+	"L4",
+	["woman", "good kisser"],
+	"School21",
+	31
+  );
+  let managerKatya = new Manager(
+	"Katya",
+	"L3",
+	["speedrunner", "good chef"],
+	"School21",
+	41
+  );
+  
+  let frontendDima = new Manager(
+	"Dima",
+	"L1",
+	["hardworking", "great dancer"],
+	"Masterbim",
+	["git", "js", "C++"],
+	21
+  );
+  
+
+// objects!
+let staffTeam = {
+	developers: {
+	  frontend: [frontendMax, frontendLeva],
+	  backend: [backendOleg],
+	},
+	managers: [managerSveta, managerKatya],
+  };
+  
+  let soLong = new Project("so_long", "L2", staffTeam);
+  let miniShell = new Project("minishell", "L3", staffTeam);
+  
+  let arrProjects = [soLong, miniShell];
+  
+  let finishedProjects = [];
+  
+  let school21 = new Company(
+	"21 school",
+	arrProjects,
+	finishedProjects,
+	staffTeam
+  );
+  
+  console.log(frontendMax);
+  frontendMax.expandStack("Unity");
+  console.log(frontendMax);
